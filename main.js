c0bd6c4150526e250873d1a4a8f26fa80998e0fe
Thomas Emerson <emersonvisuals@gmail.com>
2021-09-15 13:51:53 +1000

    mobile menu fixed

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+
+// variables 
 const trigger = document.querySelector('#toggle-trigger');
 const toggle = document.querySelector('#toggle');
 const menu = document.querySelector('#mobile-menu');
+const toggleActive = document.querySelector('#toggle.active');
+const body = document.body;
 
-
-
-console.log(trigger);
-
+// function
 trigger.onclick = function() {
     toggle.classList.toggle('active');
     menu.classList.toggle('active');
+    body.classList.toggle('active');
 }
-
